0a348d9fbd1e085d929e11c36fc497772c39b028
rishabh-997 <rishabh.agarwal997@gmail.com>
2019-06-19 00:30:31 +0530

    Login Signup

--- app/src/main/java/com/example/sorpluserend/HomePage/MVP/HomeActivity.java
+++ app/src/main/java/com/example/sorpluserend/HomePage/MVP/HomeActivity.java
@@ -1,9 +1,11 @@
 package com.example.sorpluserend.HomePage.MVP;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
+import com.example.sorpluserend.LogIn.MVP.LogInActivity;
 import com.example.sorpluserend.LogIn.MVP.LogInContract;
 import com.example.sorpluserend.LogIn.MVP.LogInPresenter;
 import com.example.sorpluserend.R;
@@ -18,5 +20,7 @@ public class HomeActivity extends AppCompatActivity implements HomeContract.view
         setContentView(R.layout.activity_main);
 
         presenter=new HomePresenter(this);
+
+        startActivity(new Intent(this, LogInActivity.class));
     }
 }
