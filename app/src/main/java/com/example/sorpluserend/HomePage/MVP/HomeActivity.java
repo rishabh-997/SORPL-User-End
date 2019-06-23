da86af41b833f6c67f2740463f181b722e9501c4
rishabh-997 <rishabh.agarwal997@gmail.com>
2019-06-23 15:23:09 +0530

    Created basic home page

--- app/src/main/java/com/example/sorpluserend/HomePage/MVP/HomeActivity.java
+++ app/src/main/java/com/example/sorpluserend/HomePage/MVP/HomeActivity.java
@@ -3,24 +3,38 @@ package com.example.sorpluserend.HomePage.MVP;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
 import com.example.sorpluserend.LogIn.MVP.LogInActivity;
-import com.example.sorpluserend.LogIn.MVP.LogInContract;
-import com.example.sorpluserend.LogIn.MVP.LogInPresenter;
 import com.example.sorpluserend.R;
 
 public class HomeActivity extends AppCompatActivity implements HomeContract.view
 {
     HomeContract.presenter presenter;
+    DrawerLayout dl;
+    ActionBarDrawerToggle abdt;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_home);
 
         presenter=new HomePresenter(this);
 
-        startActivity(new Intent(this, LogInActivity.class));
+        dl=findViewById(R.id.drawer_layout);
+        abdt=new ActionBarDrawerToggle(this,dl,R.string.Open,R.string.Close);
+        abdt.setDrawerIndicatorEnabled(true);
+        dl.addDrawerListener(abdt);
+        abdt.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        return abdt.onOptionsItemSelected(item) || super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/example/sorpluserend/LogIn/MVP/LogInActivity.java
+++ app/src/main/java/com/example/sorpluserend/LogIn/MVP/LogInActivity.java
@@ -12,6 +12,7 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.sorpluserend.HomePage.MVP.HomeActivity;
 import com.example.sorpluserend.R;
 import com.example.sorpluserend.SignUp.MVP.SignUpActivity;
 
@@ -41,14 +42,6 @@ public class LogInActivity extends AppCompatActivity implements LogInContract.vi
         presenter=new LogInPresenter(this);
         ButterKnife.bind(this);
 
-        _loginButton.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                login();
-            }
-        });
-
         _signupLink.setOnClickListener(new View.OnClickListener() {
 
             @Override
@@ -60,37 +53,22 @@ public class LogInActivity extends AppCompatActivity implements LogInContract.vi
                 overridePendingTransition(R.anim.push_left_in, R.anim.push_left_out);
             }
         });
+
+        _loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                login();
+            }
+        });
+
     }
 
-    public void login() {
-        Log.d(TAG, "Login");
-
-        if (!validate()) {
-            onLoginFailed();
-            return;
-        }
-
-        _loginButton.setEnabled(false);
-
-        final ProgressDialog progressDialog = new ProgressDialog(LogInActivity.this);
-        progressDialog.setIndeterminate(true);
-        progressDialog.setMessage("Authenticating...");
-        progressDialog.show();
-
-        String email = _emailText.getText().toString();
-        String password = _passwordText.getText().toString();
-
-        // TODO: Implement your own authentication logic here.
-
-        new android.os.Handler().postDelayed(
-                new Runnable() {
-                    public void run() {
-                        // On complete call either onLoginSuccess or onLoginFailed
-                        onLoginSuccess();
-                        // onLoginFailed();
-                        progressDialog.dismiss();
-                    }
-                }, 3000);
+    private void login()
+    {
+        Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
+        startActivityForResult(intent, REQUEST_SIGNUP);
+        finish();
+        overridePendingTransition(R.anim.push_left_in, R.anim.push_left_out);
     }
 
 
@@ -106,43 +84,4 @@ public class LogInActivity extends AppCompatActivity implements LogInContract.vi
         }
     }
 
-    @Override
-    public void onBackPressed() {
-        // Disable going back to the MainActivity
-        moveTaskToBack(true);
-    }
-
-    public void onLoginSuccess() {
-        _loginButton.setEnabled(true);
-        finish();
-    }
-
-    public void onLoginFailed() {
-        Toast.makeText(getBaseContext(), "Login failed", Toast.LENGTH_LONG).show();
-
-        _loginButton.setEnabled(true);
-    }
-
-    public boolean validate() {
-        boolean valid = true;
-
-        String email = _emailText.getText().toString();
-        String password = _passwordText.getText().toString();
-
-        if (email.isEmpty() || !android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
-            _emailText.setError("enter a valid email address");
-            valid = false;
-        } else {
-            _emailText.setError(null);
-        }
-
-        if (password.isEmpty() || password.length() < 4 || password.length() > 10) {
-            _passwordText.setError("between 4 and 10 alphanumeric characters");
-            valid = false;
-        } else {
-            _passwordText.setError(null);
-        }
-
-        return valid;
-    }
 }
